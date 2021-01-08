8c9cfb5ebd445dbfa68d05964351bd723be8b10f
Sebastian Saueressig <s.saueressig@sotec.eu>
2021-01-08 11:13:58 +0100

    "Added some test cases - playing around"

--- hello.go
+++ hello.go
@@ -4,16 +4,28 @@ import "fmt"
 
 //Constants are defined like so
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func hello(input string) string {
+func hello(input string, language string) string {
+
 	if input == "" {
 		input = "World"
 	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + input
+	}
+
+	if language == "French" {
+		return frenchHelloPrefix + input
+	}
+
 	return englishHelloPrefix + input
 }
 
 func main() {
 
-	fmt.Println(hello(""))
+	fmt.Println(hello("", ""))
 
 }
--- hello_test.go
+++ hello_test.go
@@ -1,3 +1,7 @@
+package main
+
+import "testing"
+
 /*
 Writing a test is just like writing a function, with a few rules
 	- It needs to be in a file with a name like xxx_test.go
@@ -7,25 +11,38 @@ Writing a test is just like writing a function, with a few rules
 
 func TestHello(t *testing.T) {
 
-    t.Run("saying hello to people", func(t *testing.T) {
-        got := Hello("Chris")
+	//We need to pass in t *testing.T so that we can tell the test code to fail when we need to.
+	//t.Helper() is needed to tell the test suite that this method is a helper.
+	//By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
-    })
+	})
 
-    t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-        got := Hello("")
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
-    })
+	})
 
-	//We need to pass in t *testing.T so that we can tell the test code to fail when we need to.
-	//t.Helper() is needed to tell the test suite that this method is a helper. 
-	//By doing this when it fails the line number reported will be in our function call rather than inside our test helper. 
-    assertCorrectMessage := func(t *testing.T, got, want string) {
-        t.Helper()
-        if got != want {
-            t.Errorf("got %q want %q", got, want)
-        }
-    }
+	t.Run("in Spanish", func(t *testing.T) {
+		got := hello("Carlo", "Spanish")
+		want := "Hola, Carlo"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := hello("Elo", "French")
+		want := "Bonjour, Elo"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
